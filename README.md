ff5b755959fe4a4adf534f8dcaecbea57e3c3639
Brandon <69353222+BrandonCanCode@users.noreply.github.com>
2024-03-06 16:23:04 -0500

    Revert "Merged the project into a python project and added motion sensor support"

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Utilizes an intel depth camera and a raspberry pi 5 to control an autonomous nerf gun turret.
 
 # Dependencies
-*
+* SPD Logger: sudo apt install libspdlog-dev
---
+++ cv_lib/test_cv_main.cpp
@@ -0,0 +1,12 @@
+/* test_cv_main.cpp
+ * Used to run tests on the computer vision (cv) library
+*/
+
+#include "computer_vision.h"
+
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
